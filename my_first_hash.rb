f490bcc1c140dd5692cd310d9c8feedacf78e364
Temitope Akintoye <ruth_olukunle@yahoo.com>
2018-04-29 21:28:23 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,10 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
+  my_hash = {
+    "name" => "Temitope", "Edwin", "Adora"
+    "family role" => "Mother", "Father", "First Child"
+    "occupation" => "Analyst", "Consultant", "baby"
+  }
   
 end
 
